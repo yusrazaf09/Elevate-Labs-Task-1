f5e5df4c7b0c28f264b158c9a85981630520c9eb
Yusra Zafar <125033594+yusrazaf09@users.noreply.github.com>
2025-09-22 20:54:55 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,7 +21,7 @@ public class Main {
     public static double divide(double a, double b) {
         if (b == 0) {
             System.out.println("Error! Division by zero.");
-            return 0.0; 
+            return Double.NaN; 
         }
         return a / b;
     }
